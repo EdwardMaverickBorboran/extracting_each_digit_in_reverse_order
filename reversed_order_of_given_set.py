bca97b833184383c0aad95e91d3756d49b7da58c
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-23 02:33:50 +0800

    Added the set of numbers and created a variable for it. Display the output.

---
+++ reversed_order_of_given_set.py
@@ -0,0 +1,12 @@
+# Create a program that will --
+
+# Extract each digit from an integer in the reverse order.
+# For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# Given set of numbers
+set_number = 7563
+print("The set of number is:", set_number)
